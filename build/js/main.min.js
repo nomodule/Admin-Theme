2e4da2fb6afbcd3dade9cf41ab54aed04dce4302
-HOME-PC\-HOME <ajay.rawat.oditi@gmail.com>
2018-03-16 11:46:11 +0530

    added alert

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -1,32 +1,8 @@
 "use-strict";
 
-/**
- * ------------------------------------------------------------------------
- * #NAVBAR
- * ------------------------------------------------------------------------
- */
-;(function(){
-	var cNavbarLeftHeader = document.querySelector('.navbar-left__header'),
-			cNavbarLeftMenu = document.querySelector('.navbar-left__menu')
-			cNavbarLeftToggler = document.querySelector('.navbar-left__toggler');
-
-	cNavbarLeftToggler.addEventListener('click', function(){
-		cNavbarLeftMenu.classList.toggle('open');
-	});
-})();
-
-
-/**
- * ------------------------------------------------------------------------
- * #DROPDOWN
- * ------------------------------------------------------------------------
- */
 (function(){
-  var cDropdowns = document.getElementsByClassName('dropdown'),
-      cDropdownTogglers = document.getElementsByClassName('dropdown-toggler'),
-      cDropdownMenus = document.getElementsByClassName('dropdown-menu'),
-      cDropdownItem = document.getElementsByClassName('dropdown-item');
 
+  // #GLOBAL
 
   function attachClickEventOnArrayItems(arr, fn) {
     for (var i = 0; i < arr.length; i++) {
@@ -34,34 +10,94 @@
     }
   }
 
-  function dropDownMenuToggler() {
-    for (var i = 0; i < cDropdownMenus.length; i++) {
-      if (this === cDropdownTogglers[i]) {
-        cDropdownMenus[i].classList.toggle('show');
-      } else {
-        cDropdownMenus[i].classList.remove('show');
-      }
-    }
-  }
+  /**
+   * ------------------------------------------------------------------------
+   * #NAVBAR
+   * ------------------------------------------------------------------------
+   */
+  ;(function(){
+  	var cNavbarLeftHeader = document.querySelector('.navbar-left__header'),
+  			cNavbarLeftMenu = document.querySelector('.navbar-left__menu')
+  			cNavbarLeftToggler = document.querySelector('.navbar-left__toggler');
 
-  function hideDropdown() {
-    this.parentNode.classList.remove('show');
-  }
+  	cNavbarLeftToggler.addEventListener('click', function(){
+  		cNavbarLeftMenu.classList.toggle('open');
+  	});
+  })();
 
-  attachClickEventOnArrayItems(cDropdownItem, hideDropdown);
 
-  attachClickEventOnArrayItems(cDropdownTogglers, dropDownMenuToggler);
+  /**
+   * ------------------------------------------------------------------------
+   * #DROPDOWN
+   * ------------------------------------------------------------------------
+   */
+  (function(){
+    var cDropdowns = document.getElementsByClassName('dropdown'),
+        cDropdownTogglers = document.getElementsByClassName('dropdown-toggler'),
+        cDropdownMenus = document.getElementsByClassName('dropdown-menu'),
+        cDropdownItem = document.getElementsByClassName('dropdown-item');
 
-  // clicked outside dropdown menu handler
-  function clickedOutsideDropdownMenu() {
-    document.addEventListener('click', function(e){
-      if (!e.target.classList.contains('dropdown-toggler')) {
-        for (var i = 0; i < cDropdownMenus.length; i++) {
+
+    // function attachClickEventOnArrayItems(arr, fn) {
+    //   for (var i = 0; i < arr.length; i++) {
+    //     arr[i].addEventListener('click', fn);
+    //   }
+    // }
+
+    function dropDownMenuToggler() {
+      for (var i = 0; i < cDropdownMenus.length; i++) {
+        if (this === cDropdownTogglers[i]) {
+          cDropdownMenus[i].classList.toggle('show');
+        } else {
           cDropdownMenus[i].classList.remove('show');
         }
       }
-    });
-  }
+    }
 
-  clickedOutsideDropdownMenu();
+    function hideDropdown() {
+      this.parentNode.classList.remove('show');
+    }
+
+    attachClickEventOnArrayItems(cDropdownItem, hideDropdown);
+
+    attachClickEventOnArrayItems(cDropdownTogglers, dropDownMenuToggler);
+
+    // clicked outside dropdown menu handler
+    function clickedOutsideDropdownMenu() {
+      document.addEventListener('click', function(e){
+        if (!e.target.classList.contains('dropdown-toggler')) {
+          for (var i = 0; i < cDropdownMenus.length; i++) {
+            cDropdownMenus[i].classList.remove('show');
+          }
+        }
+      });
+    }
+
+    clickedOutsideDropdownMenu();
+  })();
+
+
+  /**
+   * ------------------------------------------------------------------------
+   * #ALERTS
+   * ------------------------------------------------------------------------
+   */
+
+  (function(){
+    var cAlerts = document.getElementsByClassName('alert'),
+        cAlertCloseButtons = document.getElementsByClassName('alert-close');
+
+        console.log(cAlerts);
+        console.log(cAlertCloseButtons);
+
+
+    function hideAlert() {
+      // this.parentNode.classList.add('alert--hidden');
+      this.parentNode.outerHTML = "";
+    }
+
+    attachClickEventOnArrayItems(cAlertCloseButtons, hideAlert);
+
+  })();
+
 })();
